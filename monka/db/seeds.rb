ba18794a3d27a19c9791c5b78b4ebc3a8421c74d
shoya sakaguchi <shouya_sakaguchi@pure-system.com>
2017-01-20 05:16:35 +0000

    seeds.rbでマスター、トランザクションデータ作成

--- monka/db/seeds.rb
+++ monka/db/seeds.rb
@@ -5,3 +5,22 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+require 'csv'
+CSV.foreach('db/tags.csv', headers: :first_row) do |row|
+tag = Tag.find_or_create_by(name: row['name'])
+end
+CSV.foreach('db/books.csv', headers: :first_row) do |row|
+book = Book.find_or_create_by(title: row['title'], author: row['author'],
+price: row['price'])
+book.published_on = Date.parse(row['published_on'])
+book.save
+unless row['tag_name1'].blank?
+tag = Tag.where(name: row['tag_name1']).first
+tagging = Tagging.find_or_create_by(book_id: book.id, tag_id: tag.id)
+end
+unless row['tag_name2'].blank?
+tag = Tag.where(name: row['tag_name2']).first
+tagging = Tagging.find_or_create_by(book_id: book.id, tag_id: tag.id)
+end
+end
