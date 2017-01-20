8cb745f44cfadcae560e613265ebf75cdf727256
shoya sakaguchi <shouya_sakaguchi@pure-system.com>
2017-01-20 08:59:16 +0000

    seeds更新

--- monka/db/seeds.rb
+++ monka/db/seeds.rb
@@ -8,19 +8,25 @@
 
 require 'csv'
 CSV.foreach('db/tags.csv', headers: :first_row) do |row|
-tag = Tag.find_or_create_by(name: row['name'])
+    tag = Tag.find_or_create_by(name: row['name'])
 end
 CSV.foreach('db/books.csv', headers: :first_row) do |row|
-book = Book.find_or_create_by(title: row['title'], author: row['author'],
-price: row['price'])
-book.published_on = Date.parse(row['published_on'])
-book.save
-unless row['tag_name1'].blank?
-tag = Tag.where(name: row['tag_name1']).first
-tagging = Tagging.find_or_create_by(book_id: book.id, tag_id: tag.id)
+    book = Book.find_or_create_by(title: row['title'], author: row['author'],
+    price: row['price'])
+    book.published_on = Date.parse(row['published_on'])
+    book.save
+    unless row['tag_name1'].blank?
+        tag = Tag.where(name: row['tag_name1']).first
+        tagging = Tagging.find_or_create_by(book_id: book.id, tag_id: tag.id)
+    end
+    unless row['tag_name2'].blank?
+        tag = Tag.where(name: row['tag_name2']).first
+        tagging = Tagging.find_or_create_by(book_id: book.id, tag_id: tag.id)
+    end
 end
-unless row['tag_name2'].blank?
-tag = Tag.where(name: row['tag_name2']).first
-tagging = Tagging.find_or_create_by(book_id: book.id, tag_id: tag.id)
+
+admin = User.where(email: 'test1@example.com')
+unless admin.present?
+    User.create(name: 'Admin', email: 'test1@example.com', password: 'hoge1', password_confirmation: 'hoge1')
 end
-end
+
