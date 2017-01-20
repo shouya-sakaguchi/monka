5cba6d045529d609aace50920454ec4dc65453c3
shoya sakaguchi <shouya_sakaguchi@pure-system.com>
2017-01-20 05:55:44 +0000

    routes.rb設定

--- monka/config/routes.rb
+++ monka/config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
-  resources :taggings
-  resources :tags
-  resources :books
+  resources :tags, except: :show
+  resources :books do
+    resources :taggings, only: [:new, :edit, :create, :update, :destroy]
+  end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
