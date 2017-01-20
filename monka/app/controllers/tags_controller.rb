5e2bcbc0ed7d39e6245b4064b61783be2a4259d7
shoya sakaguchi <shouya_sakaguchi@pure-system.com>
2017-01-20 06:22:41 +0000

    データ作成成功後のリダイレクト方法の修正

--- monka/app/controllers/tags_controller.rb
+++ monka/app/controllers/tags_controller.rb
@@ -28,7 +28,7 @@ class TagsController < ApplicationController
 
     respond_to do |format|
       if @tag.save
-        format.html { redirect_to @tag, notice: 'Tag was successfully created.' }
+        format.html { redirect_to tags_url, notice: 'Tag was successfully created.' }
         format.json { render :show, status: :created, location: @tag }
       else
         format.html { render :new }
