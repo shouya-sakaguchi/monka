67c4298fd5e4b5a7065fa829f61cfec69372dd33
shoya sakaguchi <shouya_sakaguchi@pure-system.com>
2017-01-24 04:00:55 +0000

    モデル修正

--- monka/app/models/book.rb
+++ monka/app/models/book.rb
@@ -1,3 +1,4 @@
 class Book < ApplicationRecord
-    has_many :tagging
+    has_many :taggings
+    has_many :tags
 end
--- monka/app/models/tag.rb
+++ monka/app/models/tag.rb
@@ -1,3 +1,4 @@
 class Tag < ApplicationRecord
-    has_many :tagging
+    has_many :taggings
+    has_many :books
 end
